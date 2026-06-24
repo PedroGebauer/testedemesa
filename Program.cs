70874ed61de306e327140efe5c4ef2ab69cc58e2
Pedro Machado <pedromgebauer@gmail.com>
2026-06-24 12:00:02 -0300

    git final

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Programa
+{
+    static void Main()
+    {
+        int a = 2;
+        int[] vetor = new int[6];
+
+        while (a < 6)
+        {
+            vetor[a] = 10 * a;
+            Console.WriteLine(vetor[a]);
+            a += 1;
+        }
+    }
+}
