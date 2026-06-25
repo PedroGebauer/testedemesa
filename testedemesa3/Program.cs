7453f060c4a12c2b689c37fd55b8b9308116a151
Pedro Machado Gebauer <pedromgebauer@gmail.com>
2026-06-25 12:48:28 -0300

    Add files via upload

---
+++ testedemesa3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Programa
+{
+    static void Main()
+    {
+        int a, b;
+
+        a = 7;
+        b = a - 6;
+
+        int[] v = new int[6];
+
+        while (b < 6)
+        {
+            v[b] = b + a;
+            Console.WriteLine(v[b]);
+            b = b + 2;
+        }
+    }
+}
