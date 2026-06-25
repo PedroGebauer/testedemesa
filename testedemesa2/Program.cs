5ae7189f4d6f167daa81d01115425aa5c2db3d75
Pedro Machado Gebauer <pedromgebauer@gmail.com>
2026-06-25 12:47:25 -0300

    Add files via upload

---
+++ testedemesa2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Programa
+{
+    static void Main()
+    {
+        int a, b, c;
+
+        a = 10;
+        b = 20;
+        c = (a + b) / 2;
+        c = c - 40;
+
+        int[] v = new int[4];
+        v[3] = a + b + c;
+
+        Console.WriteLine($"a = {a}, b = {b}, c = {c}");
+        Console.WriteLine($"v[3] = {v[3]}");
+    }
+}
